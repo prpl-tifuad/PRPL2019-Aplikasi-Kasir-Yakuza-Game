f387a2d7b3b1179cb06f695f12fe61a69640e709
GilangRamadan <40588613+gilangramadn@users.noreply.github.com>
2019-05-05 00:21:59 +0700

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
- include 'koneksi.php';
-
-$nomor_pc = $_GET['id'];
-
-mysqli_query($koneksi,"DELETE FROM prosesbayar where nomor_pc = '$nomor_pc'");
-// die("testing");
-header("location:datatransaksi.php");
- ?>
